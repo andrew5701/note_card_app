2b8e3af1e0e172dc407c5e73112fb2e918875ed5
Andrew <andrew76280@gmail.com>
2024-06-07 21:01:21 -0400

    feat: Updated flashcard appearance

--- lib/components/flashcard_view.dart
+++ lib/components/flashcard_view.dart
@@ -1,8 +1,14 @@
+import 'dart:math';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flip_card/flip_card.dart';
+import 'package:flutter/widgets.dart';
+import 'package:note_card_app/card_operations/delete_card.dart';
 import 'package:note_card_app/card_operations/new_card.dart';
+import 'package:note_card_app/card_operations/update_card.dart';
+import 'package:note_card_app/components/flashcard_groups.dart';
 
 class CardView extends StatelessWidget {
   final String text;
@@ -16,7 +22,11 @@ class CardView extends StatelessWidget {
       child: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Text(text),
+          child: Text(text,
+              style: const TextStyle(
+                fontSize: 20,
+              ),),
+
         ),
       ),
     );
@@ -34,7 +44,7 @@ class FlashcardView extends StatefulWidget {
 }
 
 class _FlashcardViewState extends State<FlashcardView> {
-  List<Map<String, String>> flashcards = [];
+  List<Map<String, dynamic>> flashcards = [];
   String errorMessage = '';
   int currentIndex = 0;
 
@@ -58,7 +68,9 @@ class _FlashcardViewState extends State<FlashcardView> {
       QuerySnapshot querySnapshot = await FirebaseFirestore.instance
           .collection('users')
           .doc(userId)
-          .collection(widget.collectionName)
+          .collection('flashcard_groups')
+          .doc(widget.collectionName)
+          .collection('flashcards')
           .get();
 
       if (querySnapshot.docs.isEmpty) {
@@ -66,12 +78,18 @@ class _FlashcardViewState extends State<FlashcardView> {
           errorMessage = 'No flashcards found.';
         });
       } else {
-        List<Map<String, String>> fetchedFlashcards = querySnapshot.docs.map((doc) {
+        List<Map<String, dynamic>> fetchedFlashcards =
+            querySnapshot.docs.map((doc) {
           return {
-            'front': doc['front'] as String,
-            'back': doc['back'] as String,
+            'front': doc['front'],
+            'back': doc['back'],
+            'docId': doc.id,
+            'createdAt': doc['createdAt'],
           };
         }).toList();
+
+        fetchedFlashcards.sort((a, b) => (a['createdAt'] as Timestamp)
+            .compareTo(b['createdAt'] as Timestamp));
         setState(() {
           flashcards = fetchedFlashcards;
         });
@@ -97,13 +115,17 @@ class _FlashcardViewState extends State<FlashcardView> {
 
   Widget buildAddNewCardButton() {
     return ElevatedButton(
-      onPressed: () {
-        // Navigate to add new card screen
-        Navigator.push(
+      onPressed: () async {
+        await Navigator.push(
           context,
           MaterialPageRoute(
             builder: (context) => NewCard(
               collectionName: widget.collectionName,
+              onCardAdded: () {
+                setState(() {
+                  fetchFlashcards(); // Ensure this method updates the state with new data
+                });
+              },
             ),
           ),
         );
@@ -112,15 +134,93 @@ class _FlashcardViewState extends State<FlashcardView> {
     );
   }
 
+  void handleMenuAction(String value) async {
+    if (value == 'Update Card') {
+      await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => UpdateCard(
+            front: flashcards[currentIndex]['front'] ?? '',
+            back: flashcards[currentIndex]['back'] ?? '',
+            collectionName: widget.collectionName,
+            documentName: flashcards[currentIndex]['docId'] ?? '',
+            onCardUpdated: () {
+              setState(() {
+                fetchFlashcards();
+              });
+            },
+          ),
+        ),
+      );
+    } else if (value == 'Add New Card') {
+      await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => NewCard(
+            collectionName: widget.collectionName,
+            onCardAdded: () {
+              setState(() {
+                fetchFlashcards();
+              });
+            },
+          ),
+        ),
+      );
+    } else if (value == 'Delete Card') {
+      await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => DeleteCard(
+            collectionName: widget.collectionName,
+            documentName: flashcards[currentIndex]['docId'] ?? '',
+            onCardDeleted: () {
+              setState(() {
+                fetchFlashcards();
+              });
+            },
+          ),
+        ),
+      );
+    } else if (value == 'Delete Group') {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () => Navigator.pop(context),
-        ),
-        title: const Text('Flashcard View'),
+        title: const Text('Flashcards'),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: handleMenuAction,
+            child: const Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Icon(Icons.settings),
+                Text('Settings'),
+              ],
+            ),
+            itemBuilder: (BuildContext context) {
+              return flashcards.isEmpty
+                  ? {'Add New Card', 'Delete Group'}.map((String choice) {
+                      return PopupMenuItem<String>(
+                        value: choice,
+                        child: Text(choice),
+                      );
+                    }).toList()
+                  : {
+                      'Add New Card',
+                      'Update Card',
+                      'Delete Card',
+                      'Delete Group'
+                    }.map((String choice) {
+                      return PopupMenuItem<String>(
+                        value: choice,
+                        child: Text(choice),
+                      );
+                    }).toList();
+            },
+          ),
+        ],
       ),
       body: Center(
         child: flashcards.isEmpty
@@ -132,6 +232,10 @@ class _FlashcardViewState extends State<FlashcardView> {
                         ? errorMessage
                         : 'No flashcards created yet.',
                     textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    )
                   ),
                   const SizedBox(height: 20),
                   buildAddNewCardButton(),
@@ -155,17 +259,57 @@ class _FlashcardViewState extends State<FlashcardView> {
                     ),
                   ),
                   const SizedBox(height: 20),
+                  Container(
+                    margin: const EdgeInsets.only(top: 50),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "${currentIndex + 1} / ${flashcards.length}",
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      IconButton(
-                        icon: const Icon(Icons.arrow_left),
-                        onPressed: showPreviousCard,
+                      Container(
+                        margin: const EdgeInsets.only(top: 60, right: 20),
+                        decoration: const BoxDecoration(
+                          border: Border(
+                            top: BorderSide(width: 2.0, color: Colors.white),
+                            left: BorderSide(width: 2.0, color: Colors.white),
+                            bottom: BorderSide(width: 2.0, color: Colors.white),
+                            right: BorderSide(width: 2.0, color: Colors.white),
+                          ),
+                        ),
+                        child: IconButton(
+                          iconSize: 120,
+                          color: Colors.white,
+                          icon: const Icon(Icons.arrow_left),
+                          onPressed: showPreviousCard,
+                        ),
                       ),
-                      buildAddNewCardButton(),
-                      IconButton(
-                        icon: const Icon(Icons.arrow_right),
-                        onPressed: showNextCard,
+                      Container(
+                        margin: const EdgeInsets.only(top: 60, left: 20),
+                        decoration: const BoxDecoration(
+                          border: Border(
+                            top: BorderSide(width: 2.0, color: Colors.white),
+                            left: BorderSide(width: 2.0, color: Colors.white),
+                            bottom: BorderSide(width: 2.0, color: Colors.white),
+                            right: BorderSide(width: 2.0, color: Colors.white),
+                          ),
+                        ),
+                        child: IconButton(
+                          iconSize: 120,
+                          color: Colors.white,
+                          icon: const Icon(Icons.arrow_right),
+                          onPressed: showNextCard,
+                        ),
                       ),
                     ],
                   ),
